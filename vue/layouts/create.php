ecf68a6dc56614538c20e6dc0a28e5e0f4609a29
unknown <jabrikaoutar1@gmail.com>
2021-05-25 02:05:27 +0200

    final version

--- vue/layouts/create.php
+++ vue/layouts/create.php
@@ -1,26 +1,26 @@
                 <div id="popup">
-                  
+
                     <form method="POST">
 
                         <div class="mb-3">
                             <label>Machine#</label>
-                            <input type="text" name="machine" class="form-control" 
-                                    value="<?php echo $machineId; ?>" required>
+                            <input type="text" name="machine" class="form-control" value="<?php echo $machineId; ?>"
+                                required>
                         </div>
                         <div class="mb-3">
                             <label>Name</label>
-                            <input type="text" name="name" class="form-control" 
-                                    value="<?php echo $name; ?>" required>
+                            <input type="text" name="name" class="form-control" value="<?php echo $name; ?>" required>
                         </div>
                         <div class="mb-3">
                             <label>Plant</label>
-                            <input type="text" name="plant" class="form-control"  
-                                    value="<?php echo $plant; ?>" required>
+                            <input type="text" name="plant" class="form-control" value="<?php echo $plant; ?>" required>
                         </div>
                         <div class="mb-3 ">
                             <label>Status</label>
-                            <input type="text" name="status" class="form-control"  
-                                    value="<?php echo $status; ?>" required>
+                            <select name="status" class="form-control" required>
+                                <option value="active">active</option>
+                                <option value="inactive">inactive</option>
+                            </select>
                         </div>
                         <button type="submit" name="add" class="btn btn-primary">Submit</button>
                     </form>
